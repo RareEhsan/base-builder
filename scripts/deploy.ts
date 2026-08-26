68954d1150bcb6e7ddfcaf65e79c6100e7e06ef6
RareEhsan <ehsan@users.noreply.github.com>
2026-08-26 17:21:38 +0330

    wire up deploy script for Base (#3)

---
+++ scripts/deploy.ts
@@ -0,0 +1,8 @@
+import { ethers } from "ethers";
+async function main() {
+  const [signer] = await ethers.getSigners();
+  console.log("deploying from", signer.address);
+}
+main();
+
+// build note 2
