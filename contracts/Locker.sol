297672d68441053978ef895bdc13c736942db3c7
RareEhsan <ehsan@users.noreply.github.com>
2026-08-26 17:21:37 +0330

    add BaseToken ERC20 scaffold (#2)

---
+++ contracts/Locker.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+contract Locker {
+    mapping(address => uint256) public locked;
+    function lock() external payable { locked[msg.sender] += msg.value; }
+}
+
+// build note 1
